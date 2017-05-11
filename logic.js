43d1597643e400098c981404b0d6e0a599862553
Vis Vasanth <viscodesdpt@gmail.com>
2017-05-11 15:14:17 -0500

    wiki wiki wah -wiki works

--- logic.js
+++ logic.js
@@ -1,46 +1,54 @@
            $(document).ready(function() {
-               var searchTerm = "madonna";
-               var queryURL = "https://en.wikipedia.org/w/api.php?action=query&srsearch=" + searchTerm + "&format=json&callback=?";
 
-               // "https://en.wikipedia.org/w/api.php?action=query&titles=" + searchTerm + "&prop=images&imlimit=6&format=json&callback=?";
-
-               //"https://en.wikipedia.org/w/api.php?action=query&format=jsonlist=search&srsearch=" + searchTerm;
-
-               //    'http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=' + searchTerm + 'limit=6&srwhat=title&callback=?'
+               $('#searchBtn').click(function() {
+                   var searchTerm = $('#search').val();
 
 
-               $.ajax({
-                   method: "GET",
-                   url: queryURL,
-                   asyn: false,
-                   data: {
-                       action: 'query',
-                       format: 'json',
-                       list: 'search',
-                       //srwhat: 'title',
-                       srsearch: searchTerm
-                   },
-                   dataType: "json",
+                   var queryURL = "https://en.wikipedia.org/w/api.php?action=query&srsearch=" + searchTerm + "&format=json&callback=?";
+console.log(queryURL);
+                   // "https://en.wikipedia.org/w/api.php?action=query&titles=" + searchTerm + "&prop=images&imlimit=6&format=json&callback=?";
+
+                   //"https://en.wikipedia.org/w/api.php?action=query&format=jsonlist=search&srsearch=" + searchTerm;
+
+                   //    'http://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=' + searchTerm + 'limit=6&srwhat=title&callback=?'
 
 
-                   // Function to be called if the request succeeds
-                   success: function(data) {
-                       console.log(data);
+                   $.ajax({
+                       method: "GET",
+                       url: queryURL,
+                       asyn: false,
+                       data: {
+                           action: 'query',
+                           format: 'json',
+                           list: 'search',
+                           //srwhat: 'title',
+                           srsearch: searchTerm
+                       },
+                       dataType: "json",
 
-                       var resultsArray = [
-                           data.query.search[0].title,
-                           data.query.search[0].snippet,
-                           data.query.search[1].title,
-                           data.query.search[1].snippet,
-                           data.query.search[2].title,
-                           data.query.search[2].snippet,
-                           data.query.search[3].title,
-                           data.query.search[3].snippet,
-                           data.query.search[4].title,
-                           data.query.search[4].snippet
-                       ];
-                       console.log(resultsArray);
 
-                   }
+                       // Function to be called if the request succeeds
+                       success: function(data) {
+                           console.log(data);
+
+                           var resultsArray = [
+                               data.query.search[0].title,
+                               data.query.search[0].snippet,
+                               data.query.search[1].title,
+                               data.query.search[1].snippet,
+                               data.query.search[2].title,
+                               data.query.search[2].snippet,
+                               data.query.search[3].title,
+                               data.query.search[3].snippet,
+                               data.query.search[4].title,
+                               data.query.search[4].snippet
+                           ];
+                           console.log(resultsArray);
+
+//Inside the Results area, Add the appending to the HTML
+$("#results").append("<p>" + resultsArray);
+
+                       }
+                   });
                });
            });
